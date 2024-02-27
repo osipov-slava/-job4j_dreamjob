aa4e494594e6561873a33ac83ee588d09f0b4608
Slava Osipov <skybright@mail.ru>
2024-02-27 22:00:40 +0300

    task_504841

---
+++ src/main/java/ru/job4j/dreamjob/model/Vacancy.java
@@ -0,0 +1,49 @@
+package ru.job4j.dreamjob.model;
+
+import java.util.Objects;
+
+public class Vacancy {
+
+    private int id;
+
+    private String title;
+
+    public Vacancy(int id, String title) {
+        this.id = id;
+        this.title = title;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Vacancy vacancy = (Vacancy) o;
+        return id == vacancy.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+}
