0caa52d01d72bceaebee4d90e572881117c9c85b
Slava Osipov <skybright@mail.ru>
2024-02-29 19:46:46 +0300

    task_504844

--- src/main/java/ru/job4j/dreamjob/controller/VacancyController.java
+++ src/main/java/ru/job4j/dreamjob/controller/VacancyController.java
@@ -18,4 +18,9 @@ public class VacancyController {
         model.addAttribute("vacancies", vacancyRepository.findAll());
         return "vacancies/list";
     }
+
+    @GetMapping("/create")
+    public String getCreationPage() {
+        return "vacancies/create";
+    }
 }
