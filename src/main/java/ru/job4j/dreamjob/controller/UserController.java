cf80ad6debea84c7edade8fcd1ec75016e27681e
Slava Osipov <skybright@mail.ru>
2024-03-09 02:48:15 +0300

    task_504860

---
+++ src/main/java/ru/job4j/dreamjob/controller/UserController.java
@@ -0,0 +1,37 @@
+package ru.job4j.dreamjob.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import ru.job4j.dreamjob.model.User;
+import ru.job4j.dreamjob.service.UserService;
+
+@Controller
+@RequestMapping("/users")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/register")
+    public String getRegistrationPage() {
+        return "users/register";
+    }
+
+    @PostMapping("/register")
+    public String register(@ModelAttribute User user, Model model) {
+        System.out.println(user.toString());
+        var savedUser = userService.save(user);
+        if (savedUser.isEmpty()) {
+            model.addAttribute("message", "Пользователь с такой почтой уже существует");
+            return "errors/404";
+        }
+        return "redirect:/vacancies";
+    }
+}
---
+++ src/main/java/ru/job4j/dreamjob/repository/Sql2oUserRepository.java
@@ -0,0 +1,67 @@
+package ru.job4j.dreamjob.repository;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Repository;
+import org.sql2o.Sql2o;
+import org.sql2o.Sql2oException;
+import ru.job4j.dreamjob.model.User;
+
+import java.util.Collection;
+import java.util.Optional;
+
+@Slf4j
+@Repository
+public class Sql2oUserRepository implements UserRepository {
+
+    private final Sql2o sql2o;
+
+    public Sql2oUserRepository(Sql2o sql2o) {
+        this.sql2o = sql2o;
+    }
+
+    @Override
+    public Optional<User> save(User user) {
+        try (var connection = sql2o.open()) {
+            var sql = """
+                      INSERT INTO users (email, name, password)
+                      VALUES (:email, :name, :password)
+                      """;
+            var query = connection.createQuery(sql, true)
+                    .addParameter("email", user.getEmail())
+                    .addParameter("name", user.getName())
+                    .addParameter("password", user.getPassword());
+            int generatedId = query.executeUpdate().getKey(Integer.class);
+            user.setId(generatedId);
+            return Optional.of(user);
+        } catch (Sql2oException e) {
+            log.error("User с таким email уже существует");
+        }
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<User> findByEmailAndPassword(String email, String password) {
+        try (var connection = sql2o.open()) {
+            var query = connection.createQuery("SELECT * FROM users WHERE (email = :email AND password = :password)");
+            query.addParameter("email", email);
+            query.addParameter("password", password);
+            var user = query.executeAndFetchFirst(User.class);
+            return Optional.ofNullable(user);
+        }
+    }
+
+    public Collection<User> findAll() {
+        try (var connection = sql2o.open()) {
+            var query = connection.createQuery("SELECT * FROM users");
+            return query.executeAndFetch(User.class);
+        }
+    }
+
+    public boolean deleteById(int id) {
+        try (var connection = sql2o.open()) {
+            var query = connection.createQuery("DELETE FROM users WHERE id = :id");
+            query.addParameter("id", id);
+            return query.executeUpdate().getResult() != 0;
+        }
+    }
+}
